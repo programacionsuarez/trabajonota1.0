4384a73313f47e3f546e64705d5f9a4bd2a10745
programacionsuarez <juanp.suarezc@uqvirtual.edu.co>
2024-11-04 14:16:32 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package co.edu.uniquindio.poo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("view/ReservaView.fxml"));
+            primaryStage.setTitle("Sistema de Reservas de Vehículos");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
